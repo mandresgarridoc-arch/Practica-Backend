e6e82fe50abc0071963e933545054d28229c5756
m.garrido <m.andres.garridoc@gmail.com>
2026-08-24 00:59:12 -0400

    Modifico error en ruta de muestreo de hora

--- terceraApp/urls.py
+++ terceraApp/urls.py
@@ -5,6 +5,7 @@ from terceraApp import views
 urlpatterns = [
     #Para se muestre en la primera pagina la ruta debe quedar vacia 
         # y tambien debe quedar vacia en el archivo de url principal
-    path('', views.inicio_tercera),
+    path('', views.hora_actual),
+    path('dado/', views.dado),
     
 ]
--- terceraApp/views.py
+++ terceraApp/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 import datetime
+import random
 
 # Create your views here.
 
 def hora_actual(request):
-    hora = datetime.datetime.now()
-    return render(request, 'index.html', {'hora': hora})
+    hora_ahora = datetime.datetime.now()
+    contexto = {'hora': hora_ahora}
+    return render(request, 'index.html', contexto)
 
-def inicio_tercera(request):
+
+"""def inicio_tercera(request):
     # Aqui solo llamo al archivo con render
-    return render(request, 'index.html')
+    return render(request, 'index.html')"""
+
+def dado(request):
+    numero = random.randint(1,6)
+    return render(request, 'dado.html', {'numero': numero})
